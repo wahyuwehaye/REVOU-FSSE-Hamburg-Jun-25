f4f8f30e772a74b8d1950590b0f7ca1a7f275de1
Wahyu Saepuloh <wehaye94@gmail.com>
2025-10-21 22:34:44 +0700

    testing

--- week17/examples/nextjs-testing-demo/src/__tests__/sum.test.ts
+++ week17/examples/nextjs-testing-demo/src/__tests__/sum.test.ts
@@ -8,4 +8,14 @@ describe('sum()', () => {
   it('mendukung angka negatif', () => {
     expect(sum(-2, 3)).toBe(1);
   });
+
+  it('menginputkan string yang berisi angka', () => {
+    expect(sum(Number('4'), Number('5'))).toBe(9);
+  });
+
+  it('menghasilkan NaN jika input bukan angka', () => {
+    // expect(sum(Number('a'), 3)).toBeNaN();
+    expect(sum(Number('a'), 3)).toBe(3);
+  });
+
 });
